08edb3197f4b0f0563231ffc6d5c201f8e014853
SlowSpeedChase <58612484+SlowSpeedChase@users.noreply.github.com>
2021-11-09 21:34:06 -0600

    solved grouping and rounding!

--- main.py
+++ main.py
@@ -7,21 +7,29 @@
 import pandas as pd
 import numpy as np
 
-# O1
+# Create my data frame
 health_data = pd.read_csv('insurance.csv')
+
+# O1
 avg_age = int(health_data['age'].mean())
+
 # O2
 most_populous_region = health_data['region'].mode()
+
 # O3
 # df.loc[:,[(df[col] > 14).any() for col in df.columns]]
-print(health_data)
-print(health_data.head())
+# a = health_data.loc[:,["smoker"]  'yes']
+# a = health_data['smoker'].str.contains('yes')  # returns true if smoker = yes
+# Seems related https://stackoverflow.com/questions/46307490/how-can-i-extract-the-nth-row-of-a-pandas-data-frame-as-a-pandas-data-frame/46307819
+smoker_avgcost = round(health_data.loc[health_data['smoker'] == 'yes'].groupby('smoker').charges.mean(),2)
+nonsmoker_avgcost = round(health_data.loc[health_data['smoker'] == 'no'].groupby('smoker').charges.mean(),2)
+
+
 
 # O4
-
 
 
 # Output
 print('Patients average age is ' + str(avg_age) + '.')
 print('Most patients are from the ' + most_populous_region)
-
+print(f"Smokers cost (on average) ${nonsmoker_avgcost}, and Non-smokers cost (on average) ${smoker_avgcost}. It costs {nonsmoker_avgcost}-{smoker_avgcost} less to not smoke!")
